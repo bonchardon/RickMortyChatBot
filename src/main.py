49d54d6c918bbf62a0364b77bfa694d0f88dd900
mborodada <mboroda@lytvynov-production.com>
2024-11-20 12:05:19 +0200

    .env.sampel

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-import requests
-from urllib.parse import urljoin
-from bs4 import BeautifulSoup
+from typing import Final
+
 import time
 import random
+from os import environ
+from dotenv import load_dotenv
+
+from requests import get
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
 from telegram import Bot
 from aiogram import Bot, types
 from telegram import Update
 from itertools import chain
-from typing import Final
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler, ContextTypes, filters
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
@@ -15,46 +20,28 @@ import aiogram
 import schedule
 import threading
 
-TOKEN: Final = '6475998403:AAHHPMW8HWxz1zwL5diBbPeaLKGCs2fUE5k'
-BOT_USERNAME: Final = '@RiclMortyBot'
+load_dotenv()
 
-# bot = Bot(token=TOKEN)
-# # dp = aiogram.dispatcher(bot)
-
-
-# Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(update.message.chat_id)
-
-    bot = Bot(token='6475998403:AAHHPMW8HWxz1zwL5diBbPeaLKGCs2fUE5k')
-
+    bot = Bot(token=environ.get('TOKEN'))
     async def scrap():
-        response = requests.get('https://simpsonsua.tv/rickandmorty-sezon-7/')
-
-        # Check if the request was successful (status code 200)
+        response = get('https://simpsonsua.tv/rickandmorty-sezon-7/')
         if response.status_code == 200:
-            # Parse the HTML content
             soup = BeautifulSoup(response.text, 'html.parser')
-
             a_discr = soup.find_all('div', class_='descr')
-            # print(a_discr)
-
             links = [a['href'] for a in soup.find_all('a', href=lambda href: href and href.startswith(
                 'https://simpsonsua.tv/rickandmorty-sezon-7/'))]
-
             exp = []
             for i in a_discr:
                 title = i.text
                 exp.append(title)
 
             combined_lines = []
-
             for i in range(0, len(exp), 2):
                 if i + 1 < len(exp):
                     combined_lines.append(exp[i] + " " + exp[i + 1])
-
             my_dict = dict(zip(combined_lines, links))
-            # print(my_dict)
             return my_dict
 
         else:
@@ -72,32 +59,20 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         all_links = []
 
-        # Iterate through each URL
         for url in urls:
-            response = requests.get(url)
-            # print(response)
-
-            # Check if the request was successful
+            response = get(url)
             if response.status_code == 200:
-                # Parse the HTML content
                 soup = BeautifulSoup(response.content, 'html.parser')
-                # print(soup)
-
                 links = [a['href'] for a in soup.find_all('a', href=lambda href: href and href.startswith(
                     'https://simpsonsua.tv/rickandmorty-sezon-'))]
                 all_links.append(links)
-
             else:
                 print("Not today, honey!")
 
         clear_list = list(chain(*all_links))
         random_episode = random.choice(clear_list)
         return random_episode
-
-    # new episode here
     scrap = await scrap()
-
-    # print random here
     random_result = await random_ep()
 
     """
@@ -106,16 +81,6 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     last_key = list(scrap)[-1]
     last_value = scrap[last_key]
-
-    # if len(scrap) > 8:
-    #     resp = f'There is a new episode already! GO AHEAD AND WATCH IT! ' \
-    #            f'Here is a link: {last_value}'
-    #     await update.message.reply_text(str(resp))
-    # else:
-    #     resp_2 = f'Not yet, soon! ' \
-    #              f'However, here is a link to one of the previous episodes: {random_result}'
-    #     await update.message.reply_text(str(resp_2))
-
     while True:
         if len(scrap) > 8:
             resp = f'There is a new episode already! GO AHEAD AND WATCH IT! ' \
@@ -140,26 +105,21 @@ async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # to inform us whether it is a private or group chat
     message_type: str = update.message.chat.type
     text: str = update.message.text
 
     print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
 
-    BOT_USERNAME = str(Bot(token='6475998403:AAHHPMW8HWxz1zwL5diBbPeaLKGCs2fUE5k'))
+    BOT_USERNAME = str(Bot(token=environ.get('BOT_USERNAME')))
 
     if message_type == 'group':
         if BOT_USERNAME in text:
             new_text: str = text.replace(BOT_USERNAME, '').strip()
-            # response: str = handle_response(new_text)
             response: str = 'HERE 1'
         else:
             return
     else:
-        # response: str = handle_response(text)
         return
-    # print('Bot', response)
-
     await update.message.reply_text(response)
 
 
@@ -168,19 +128,8 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def send_telegram_message(message):
-    # curl -s "https://api.telegram.org/bot6475998403:AAHHPMW8HWxz1zwL5diBbPeaLKGCs2fUE5k/getMe"
-    bot = Bot(token='6475998403:AAHHPMW8HWxz1zwL5diBbPeaLKGCs2fUE5k')
-    
+    bot = Bot(token=environ.get('TOKEN'))
     await bot.send_message(chat_id=6475998403, text=message)
-    # await bot.send_message(chat_id=431146613, text=message)
-    # Den
-    # await bot.send_message(chat_id=279306248, text=message)
-    # Julia
-    # await bot.send_message(chat_id=6475998403, text=message)
-
-
-# def job():
-#     schedule.every().hour.do(send_telegram_message)
 
 
 if __name__ == "__main__":
@@ -189,18 +138,11 @@ if __name__ == "__main__":
 
     app = Application.builder().token(TOKEN).build()
 
-    # Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('custom', custom_command))
 
-    # Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
-
-    # Errors
-    # app.add_handler(error)
-
-    # check constantly for any new messages
     print('Polling...')
     app.run_polling(poll_interval=5)
 
@@ -215,5 +157,3 @@ if __name__ == "__main__":
     # from aiogram import executor
     #
     # executor.start_polling(dp, skip_updates=True)
-
-    print()
